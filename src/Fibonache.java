651ab7fcac06cceff6536e82f7145762e8b068da
VladislavPonomarenko <vladponomarenko8@gmail.com>
2017-01-23 13:35:49 +0200

    fib_two

--- src/Fibonache.java
+++ src/Fibonache.java
@@ -5,20 +5,38 @@ import java.util.Scanner;
  */
 public class Fibonache {
     public static void main(String[] args) {
+
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter number: ");
         int number=scanner.nextInt();
 
-        int n0 = 1, n1 = 1, n2;
-        System.out.print(n0+" "+n1+" ");
-        for(int i = 2; i < number; i++){
+        long startTime = System.currentTimeMillis();
+//        int n0 = 1, n1 = 1, n2;
+//        System.out.print(n0+" "+n1+" ");
+       /* for(int i = 2; i < number; i++){
             n2=n0+n1;
             System.out.print(n2+" ");
             n0=n1;
             n1=n2;
         }
+        System.out.println();
+        long timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("программа выполнялась " + timeSpent + " миллисекунд");*/
+        //1000000=5487
+        int a = 1, b = 1;
+        System.out.print(a + " " + b);
+        int fibonache = 2, i = 2;
+        do{ fibonache = a + b;
+            a = b;
+            b = fibonache;
+            System.out.print(" " + fibonache);
+            i++;}while (i<number);
+        System.out.println();
+        long timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("программа выполнялась " + timeSpent + " миллисекунд");
+        //100000=5514
 
-       /* int a = 1, b = 1;
+      /*  int a = 1, b = 1;
         System.out.print(a + " " + b);
         int fibonache = 2, i = 2;
         while (i < number) {
@@ -27,6 +45,10 @@ public class Fibonache {
             b = fibonache;
             System.out.print(" " + fibonache);
             i++;
-        }*/
+        }
+        System.out.println();
+        long timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("программа выполнялась " + timeSpent + " миллисекунд");
+        //1000000=4976*/
     }
 }
